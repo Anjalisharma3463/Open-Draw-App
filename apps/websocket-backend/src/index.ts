3fbffa569133c75b4de25a0fca00f1fbdd827139
Anjali Sharma <anjalisharma1562005@gmail.com>
2025-06-15 23:26:06 +0530

    added websocket code

--- apps/websocket-backend/src/index.ts
+++ apps/websocket-backend/src/index.ts
@@ -1,17 +1,41 @@
 import { WebSocketServer } from "ws";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
+interface JwtPayload {
+  userId: string;
+}
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws) => {
+wss.on("connection", (ws , request) => {
   console.log("New client connected");
+  const url = request.url;
+  
+  // ws://localhost:3000?token=12345
+  
+  //   url = ["ws://localhost:3000", "token:12345"].join("?"); // Example URL for testing
+  if(!url) {
+    ws.close(1008, "No URL provided");
+    return;
+  }
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token");
 
+    if(!token) {
+        ws.close(1008, "No token provided");
+        return;
+    }
+   
+    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    if (!decoded) {
+        ws.close(1008, "Invalid token");
+        return;
+    }
+    console.log(`Client connected with token: ${token}`);
   ws.on("message", (message) => {
     console.log(`Received message: ${message}`);
-    // Echo the message back to the client
-    ws.send(`Server received: ${message}`);
-  });
 
-  ws.on("close", () => {
-    console.log("Client disconnected");
-  });
+    ws.send(`Echo: ${message}`);
+  }); 
 });
