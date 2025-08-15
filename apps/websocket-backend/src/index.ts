60ed970772657b7d9d0ebf82042bd74622efa7b2
Anjali sharma <anjalisharma1562005@gmail.com>
2025-08-15 23:45:46 +0530

    Handle WebSocket message types join_room and leave_room

--- apps/websocket-backend/src/index.ts
+++ apps/websocket-backend/src/index.ts
@@ -1,9 +1,53 @@
-import { WebSocketServer } from "ws";
+import ws, { WebSocketServer } from "ws";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from '@repo/backend-common/config';
 
  
 const wss = new WebSocketServer({ port: 8080 });
+
+interface User {
+  ws: ws,
+  userId: string,
+  rooms: string[]
+}
+
+
+const users: User[] = [];
+
+// user array:
+// [
+//   {
+//     ws,
+//     userId ,
+//     rooms[]
+//   },
+//   {
+//     ws,
+//     rooms: [],
+//     userId
+//   }  
+// ]
+
+
+
+
+function checkUser(token: string): string | null{
+ try {
+     const decoded = jwt.verify(token, JWT_SECRET);
+    if (typeof decoded == "string"){
+      return null;
+    }
+
+    if (!decoded) {
+        return null;
+    }
+    return decoded.userId; 
+ } catch (error) {
+  return null;
+ }
+
+ return null;
+}
 
 wss.on("connection", (ws , request) => {
   console.log("New client connected");
@@ -23,22 +67,88 @@ wss.on("connection", (ws , request) => {
         ws.close(1008, "No token provided");
         return;
     }
-   
-    const decoded = jwt.verify(token, JWT_SECRET);
+
+   const userId = checkUser(token);
+   if(!userId){
+       ws.close(1008, "Unauthorized");
+       return null;
+   }
+
+   users.push({
+    userId,
+    rooms: [],
+    ws
+   })
+    //Till now user have not joined any room thats why rooms array is empty.
+
+
+    //whenever someone wants to join a room then message come from end client is like :
     
-      if (typeof decoded == "string"){
-      ws.close(1008, "Invalid or malformed token.");
+    
+
+    // this is data in form of string
+    // {
+    //   type: "join_room",
+    //   roomId: 1
+    // }
+
+
+
+   ws.on("message", (data) => {   
+
+  const parsedData = JSON.parse(data as unknown as string)
+  
+  if(parsedData.type === "join_room"){
+    const user = users.find( x => x.ws === ws);
+    user?.rooms.push(parsedData.roomId);
+  }  
+
+  if(parsedData.type === "leave_room"){
+    const user =  users.find(x => x.ws === ws);
+    
+    if(!user){
       return;
     }
 
-    if (!decoded) {
-        ws.close(1008, "Invalid token");
-        return;
-    }
-    console.log(`Client connected with token: ${token}`);
-  ws.on("message", (message) => {
-    console.log(`Received message: ${message}`);
+    user.rooms = user.rooms.filter(x => x !== parsedData.roomId);
+  }
 
-    ws.send(`Echo: ${message}`);
-  }); 
+// {
+//   type: "chat",
+//   message:"hi there",
+//   roomId: 123
+// }
+  if (parsedData.type === "chat"){
+  const roomId = parsedData.roomId;
+  const message = parsedData.message;  
+  // check that the message that is sent to the room is exists or not in rooms array of each user.
+  users.forEach(user => {
+    if(user.rooms.includes(roomId) ){
+      user.ws.send(JSON.stringify({
+        type: "chat",
+        message: message,
+        roomId: roomId
+      }))
+    }
+  })
+  }
+
+
+// user array:
+// [
+//   {
+//     ws,
+//     userId ,
+//     rooms[]
+//   },
+//   {
+//     ws,
+//     rooms: [],
+//     userId
+//   }  
+// ]
+
+
+
+}); 
 });
